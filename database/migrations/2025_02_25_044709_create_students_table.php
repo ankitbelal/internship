423dd448ac1fef53c90dceafe1ad07ad4c0cab73
Ankit Belal <aankitbelal@gmail.com>
2025-02-25 10:34:29 +0545

    defining the column in migration file

--- database/migrations/2025_02_25_044709_create_students_table.php
+++ database/migrations/2025_02_25_044709_create_students_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            
             
         });
     }
